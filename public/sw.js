c3ef1b1e964fc024f5ca04ff08b4cf039c600481
Ajju853 <ajimp340@gmail.com>
2026-06-03 17:06:12 +0530

    feat: add Monetag head script, update sw.js zoneId to 11094850

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11094740
+    "zoneId": 11094850
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
